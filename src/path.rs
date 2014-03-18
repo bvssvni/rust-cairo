0c7031406d41ff3afc05c042bbdbb3fe6607e7c6
Sven <bvssvni@gmail.com>
2014-03-18 14:42:56 +0100

    Copied comments for path

--- src/path.rs
+++ src/path.rs
@@ -1,5 +1,20 @@
+//! Creating paths and manipulating path data
+
 use std;
 
+///  A data structure for holding a path. This data structure serves as the return value for cairo_copy_path() and cairo_copy_path_flat() as well the input value for cairo_append_path().
+///
+/// See cairo_path_data_t for hints on how to iterate over the actual data within the path.
+///
+/// The num_data member gives the number of elements in the data array. This number is larger than the number of independent path portions (defined in cairo_path_data_type_t), since the data includes both headers and coordinates for each portion.
+///
+/// cairo_status_t status; the current error status
+///
+/// cairo_path_data_t *data; the elements in the path
+///
+/// int num_data; the number of elements in the data array
+///
+/// Since 1.0
 pub struct Path {
   opaque: *mut std::libc::c_void
 }
