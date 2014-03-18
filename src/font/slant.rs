55f2e958100318056d416c63b35d5e0a8f80583b
Sven <bvssvni@gmail.com>
2014-03-18 21:08:15 +0100

    Copied documentation from Cairo for font slant

--- src/font/slant.rs
+++ src/font/slant.rs
@@ -1,7 +1,15 @@
+//! Describe font slant
+
+///  Specifies variants of a font face based on their slant.
+/// 
+/// Since 1.0
 #[repr(i32)]
 pub enum Slant {
+  /// Upright font style, since 1.0
   Normal = 0,
+  /// Italic font style, since 1.0
   Italic = 1,
+  /// Oblique font style, since 1.0
   Oblique = 2
 }
 
