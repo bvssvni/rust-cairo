2c765e2b8ed32fc4799843d550d9e7cfd06d8c92
Sven <bvssvni@gmail.com>
2014-03-18 20:57:24 +0100

    Added documentation for hint_metrics

--- src/font/hint_metrics.rs
+++ src/font/hint_metrics.rs
@@ -1,7 +1,15 @@
+//! Describe font hint metrics
+
+///  Specifies whether to hint font metrics; hinting font metrics means quantizing them so that they are integer values in device space. Doing this improves the consistency of letter and line spacing, however it also means that text will be laid out differently at different zoom factors.
+/// 
+/// Since 1.0
 #[repr(i32)]
 pub enum HintMetrics {
+  /// Hint metrics in the default manner for the font backend and target device, since 1.0
   Default = 0,
+  /// Do not hint font metrics, since 1.0
   Off = 1,
+  /// Hint font metrics, since 1.0
   On = 2
 }
 
