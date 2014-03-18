448e3877f1032f1278c5fcf81b489f15df200451
Sven <bvssvni@gmail.com>
2014-03-18 21:14:22 +0100

    Copied documentation from Cairo for font cluster flags

--- src/font/cluster_flags.rs
+++ src/font/cluster_flags.rs
@@ -1,6 +1,13 @@
+//! Describe font cluster flags
+
+///  Specifies properties of a text cluster mapping.
+/// 
+/// Since 1.8
 #[repr(i32)]
 pub enum ClusterFlags {
+  /// The clusters in the cluster array map to glyphs in the glyph array from start to end.
   Forwards = 0,
+  /// The clusters in the cluster array map to glyphs in the glyph array from end to start. (Since 1.8)
   Backwards = 1
 }
 
