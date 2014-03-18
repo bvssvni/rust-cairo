aca5fb5a9b9de1086f17254de6af316f00f9f2bd
Sven <bvssvni@gmail.com>
2014-03-18 22:45:40 +0100

    Copied Cairo documentation for line join

--- src/line_join.rs
+++ src/line_join.rs
@@ -1,20 +1,16 @@
+//! Descirbe how to render the junction of two lines
+
 ///  Specifies how to render the junction of two lines when stroking.
 /// 
 /// The default line join style is CAIRO_LINE_JOIN_MITER.
 /// 
-/// CAIRO_LINE_JOIN_MITER
-/// 	use a sharp (angled) corner, see cairo_set_miter_limit() (Since 1.0)
-/// 
-/// CAIRO_LINE_JOIN_ROUND
-/// 	use a rounded join, the center of the circle is the joint point (Since 1.0)
-///
-/// CAIRO_LINE_JOIN_BEVEL
-///	use a cut-off join, the join is cut off at half the line width from the joint point (Since 1.0)
-///
 /// Since 1.0
 #[repr(i32)]
 pub enum LineJoin {
+  /// use a sharp (angled) corner, see cairo_set_miter_limit() (Since 1.0)
   Miter = 0,
+  /// use a rounded join, the center of the circle is the joint point (Since 1.0)
   Round = 1,
+  /// use a cut-off join, the join is cut off at half the line width from the joint point (Since 1.0)
   Bevel = 2
 }
