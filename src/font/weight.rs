e432b948b197fb56c25061c380053d830fd036d1
Sven <bvssvni@gmail.com>
2014-03-18 21:08:05 +0100

    Copied documentation from Cairo for font weight

--- src/font/weight.rs
+++ src/font/weight.rs
@@ -1,6 +1,13 @@
+//! Describe font weight
+
+///  Specifies variants of a font face based on their weight.
+/// 
+/// Since 1.0i
 #[repr(i32)]
 pub enum Weight {
+  /// Normal font weight, since 1.0
   Normal = 0,
+  /// Bold font weight, since 1.0
   Bold = 1
 }
 
