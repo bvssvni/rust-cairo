c8b62766c6fe3366818430509fd89cce1ab6e0e8
Sven <bvssvni@gmail.com>
2014-03-18 22:51:32 +0100

    Copied Cairo documentation for antialias

--- src/antialias.rs
+++ src/antialias.rs
@@ -1,3 +1,5 @@
+//! Describe type of antialiasing when rendering text or shapes
+
 ///  Specifies the type of antialiasing to do when rendering text or shapes.
 ///
 /// As it is not necessarily clear from the above what advantages a particular antialias method provides, since 1.12, there is also a set of hints: CAIRO_ANTIALIAS_FAST: Allow the backend to degrade raster quality for speed CAIRO_ANTIALIAS_GOOD: A balance between speed and quality CAIRO_ANTIALIAS_BEST: A high-fidelity, but potentially slow, raster mode
@@ -6,36 +8,22 @@
 ///
 /// The interpretation of CAIRO_ANTIALIAS_DEFAULT is left entirely up to the backend, typically this will be similar to CAIRO_ANTIALIAS_GOOD.
 ///
-/// CAIRO_ANTIALIAS_DEFAULT
-///	Use the default antialiasing for the subsystem and target device, since 1.0
-///
-/// CAIRO_ANTIALIAS_NONE
-///	Use a bilevel alpha mask, since 1.0
-///
-/// CAIRO_ANTIALIAS_GRAY
-///	Perform single-color antialiasing (using shades of gray for black text on a white background, for example), since 1.0
-///
-/// CAIRO_ANTIALIAS_SUBPIXEL
-///	Perform antialiasing by taking advantage of the order of subpixel elements on devices such as LCD panels, since 1.0
-///
-/// CAIRO_ANTIALIAS_FAST
-///	Hint that the backend should perform some antialiasing but prefer speed over quality, since 1.12
-///
-/// CAIRO_ANTIALIAS_GOOD
-///	The backend should balance quality against performance, since 1.12
-///
-/// CAIRO_ANTIALIAS_BEST
-///	Hint that the backend should render at the highest quality, sacrificing speed if necessary, since 1.12
-///
 /// Since 1.0
 #[repr(i32)]
 pub enum Antialias {
+  /// Use the default antialiasing for the subsystem and target device, since 1.0
   Default = 0,
+  /// Use a bilevel alpha mask, since 1.0
   None = 1,
+  /// Perform single-color antialiasing (using shades of gray for black text on a white background, for example), since 1.0
   Gray = 2,
+  /// Perform antialiasing by taking advantage of the order of subpixel elements on devices such as LCD panels, since 1.0
   Subpixel = 3,
+  /// Hint that the backend should perform some antialiasing but prefer speed over quality, since 1.12
   Fast = 4,
+  /// The backend should balance quality against performance, since 1.12
   Good = 5,
+  ///Hint that the backend should render at the highest quality, sacrificing speed if necessary, since 1.12
   Best = 6
 }
 
