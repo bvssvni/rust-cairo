add225f9f570dfa1641062200cd7321676d2af24
Sven <bvssvni@gmail.com>
2014-03-18 22:16:40 +0100

    Copied Cairo documentation for region overlap

--- src/region/overlap.rs
+++ src/region/overlap.rs
@@ -1,6 +1,14 @@
+//! Describe region overlap.
+
+///  Used as the return value for cairo_region_contains_rectangle().
+/// 
+/// Since 1.10
 #[repr(i32)]
 pub enum Overlap {
+  /// The contents are entirely inside the region. (Since 1.10)
   In = 0,
+  /// The contents are entirely outside the region. (Since 1.10)
   Out = 1,
+  /// The contents are partially inside and partially outside the region. (Since 1.10)
   Part = 2
 }
