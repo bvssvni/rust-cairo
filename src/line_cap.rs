8cb2e42d2d8cd2a0d435efa470dad5b6076b016a
Sven <bvssvni@gmail.com>
2014-03-18 22:47:22 +0100

    Copied Cairo documentation for line cap

--- src/line_cap.rs
+++ src/line_cap.rs
@@ -1,21 +1,17 @@
+//! Describe how to render the endpoints of the path when stroking
+
 ///  Specifies how to render the endpoints of the path when stroking.
 /// 
 /// The default line cap style is CAIRO_LINE_CAP_BUTT.
 /// 
-/// CAIRO_LINE_CAP_BUTT
-///	start(stop) the line exactly at the start(end) point (Since 1.0)
-///
-/// CAIRO_LINE_CAP_ROUND
-///	use a round ending, the center of the circle is the end point (Since 1.0)
-///
-/// CAIRO_LINE_CAP_SQUARE
-///	use squared ending, the center of the square is the end point (Since 1.0)
-/// 
 /// Since 1.0
 #[repr(i32)]
 pub enum LineCap {
+  /// start(stop) the line exactly at the start(end) point (Since 1.0)
   Butt = 0,
+  /// use a round ending, the center of the circle is the end point (Since 1.0)
   Round = 1,
+  /// use squared ending, the center of the square is the end point (Since 1.0)
   Square = 2
 }
 
